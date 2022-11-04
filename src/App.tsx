d8c05fd49da60482b657c4fb1c142d7f251655c1
miyata-h256 <mhprivate79@gmail.com>
2022-11-05 07:49:29 +0900

    react_study

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import logo from './logo.svg';
+import { Child1, Child2 } from './components/Child'
 import './App.css';
 
+
+
 function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const handleIncrement = useCallback(() => {
+    setCount(prev => prev + 1)
+  }, [])
+
+  const handleDecrement = useCallback(() => {
+    setCount(prev => prev - 1)
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +23,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>{count}</div>
+        <div>
+          <button onClick={handleIncrement}>+1</button>
+          <button onClick={handleDecrement}>-1</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +36,54 @@ function App() {
         >
           Learn React
         </a>
+        <Child1 />
+        <Child2 />
       </header>
     </div>
   );
 }
+
+// type Item = {
+//   id: number
+//   title: string
+// }
+
+// type Props = {
+//   message: string
+// }
+
+// const Child: React.FC<Props> = props => {
+//   return (
+//     <p>{props.message}</p>
+//   )
+// }
+
+
+// const App: React.FC = () => {
+//   const message: string = 'Hello React!!'
+
+//   // const items: Item[] = [
+//   //   {
+//   //     id: 1,
+//   //     title: 'highest price product'
+//   //   }, {
+//   //     id: 2,
+//   //     title: 'low price product'
+//   //   }
+//   // ]
+//   return (
+//     <div className="App">
+//       <p>{message}</p>
+//       {/* <ul>
+//         {items.map((item: Item) => (
+//           <li key={item.id}>{item.title}</li>
+//         ))}
+//       </ul> */}
+//       {/* <Child message="子のコンポーネントに渡す" /> */}
+//       <Child />
+//     </div>
+//   )
+// }
+
 
 export default App;
---
+++ src/components/Child.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Child1: React.FC = () => {
+    return (
+        <div>子のコンポーネント（外部ファイル）</div>
+    )
+}
+
+
+
+export const Child2: React.FC = () => {
+    return (
+        <div>子のコンポーネント2（外部ファイル）</div>
+    )
+}
