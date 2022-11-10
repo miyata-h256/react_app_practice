896607a670a45a287b73e82d3fc03916a070947a
miyata-h256 <mhprivate79@gmail.com>
2022-11-10 23:42:06 +0900

    dropdown part2

--- src/home_components/DropDown.tsx
+++ src/home_components/DropDown.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Icon from "../img/setting.png";
 const DropDown:React.FC = () => {
     return (
-        <section>
-            <Link to="/login" className="text-[#e2e8f0] italic font-bold hover:text-shadow-purple-500 m-1">Logout</Link>
-        </section>
+        <div className="group pl-5">
+            <button type="button" className="group-hover:shadow-purple-500">
+                <img src={Icon} alt="setting" className=""/>
+            </button>
+            <div className="invisible group-hover:visible">
+                <Link to="/login" className="text-[#e2e8f0] italic font-bold hover:text-shadow-purple-500 m-1">Logout</Link>
+            </div>
+        </div>
     )
 };
 
--- src/home_components/Header.tsx
+++ src/home_components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import Icon from "../img/setting.png";
+
+import DropDown from "./DropDown";
 
 const Header:React.FC = () => {
     return (
@@ -7,20 +8,9 @@ const Header:React.FC = () => {
             <div>
                 <p className="text-5xl italic text-purple-100 text-shadow-purple-500 ">Home Page</p>
             </div>
-            <div className="flex m-2">
-                <input type="text" placeholder="Search" className="input m-1" />
-                <div id="heading">
-                <button type="button"  data-bs-toggle="collapse" className="
-                                                                           my-2
-                                                                           transition
-                                                                           ease-in-out
-                                                                           hover:delay-150
-                                                                           hover:scale-90
-                                                                           hover:translate-1"
-                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        <img src={Icon} alt="setting" className=" "/>
-                </button>
-                </div>
+            <div className="flex pt-1">
+                <input type="text" placeholder="Search" className="input" />
+                <DropDown/>
             </div>
         </header>
     )
